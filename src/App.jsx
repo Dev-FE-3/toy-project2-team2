34015fed4148888d08c8c8033628869a7e377df1
nani <pha3113@naver.com>
2025-02-20 14:33:02 +0900

    Style : globalStyle css 적용(reset, font)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Router from "./router/Router"; // 라우터 설정 불러오기
 import Layout from "./shared/Layout"; // 전체 레이아웃 적용
+import GlobalStyle from "./shared/components/styles/GlobalStyle"
 
 const App = () => {
   return (
-    <Layout>
-      <Router />
-    </Layout>
+    <>
+      <GlobalStyle />
+      <Layout>
+        <Router />
+      </Layout>
+    </>
   );
 };
 
---
+++ src/shared/components/styles/GlobalStyle.jsx
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
+import './fonts.css'
+
+const GlobalStyle = createGlobalStyle`
+	:root {
+		/* primary */
+		--primary: #00ADB5;
+		--accent: #63C5CA;
+
+		/* background-color */
+		--background-color: #EEE;
+		--disabled: #D9D9D9;
+
+		/* text-color */
+		--text-primary: #1E2A2E;
+		--text-sencondary: #4A4A4A;
+		--text-disabled: #828282;
+
+		/* font-size */
+		--font-size-title-large: 32px;
+		--font-size-title-medium: 24px;
+		--font-size-title-small: 18px;
+		--font-size-primary: 16px;
+		--font-size-samll: 14px;
+	}
+
+	// reset css
+	${reset}
+
+	body {
+		font-family: 'Noto Sans KR', sans-serif;
+	}
+`
+
+export default GlobalStyle
