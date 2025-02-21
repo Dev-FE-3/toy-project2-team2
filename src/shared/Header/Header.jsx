d407a63ab9574c470c7ee470473fb5a95c50c727
PARK HYOEN AH <pha3113@naver.com>
2025-02-21 13:53:24 +0900

    Design : header 적용 (#1)

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,18 +1,46 @@
 import styled from "styled-components";
+import logo from './../../assets/images/logo.svg';
 import LogOutButton from "./LogOutButton";
 import Nav from "./Nav";
+import { Link } from "react-router-dom";
 
-const Menu = styled.div``;
-const MenuItem = styled.div``;
+const HeaderWrap = styled.header`
+  display: flex;
+  justify-content: space-between;
+  padding: 18px 20px;
+  background-color: #fff;
+`
+
+const Logo = styled.img`
+  width: 112px;
+  height: 100%;
+`
+
+const Wrap = styled.div`
+  display: flex;
+  gap: 60px;
+`
+
+const User = styled.span`
+  display: inline-block;
+  font-size: 16px;
+  font-weight: 400;
+  color: var(--text-primary);
+  line-height: 38px;
+`
 
 const Header = () => {
   return (
-    <Menu>
-      <MenuItem>svg 넣는 칸</MenuItem>
-      <Nav />
-      <MenuItem>김이삭 메이트</MenuItem>
-      <LogOutButton />
-    </Menu>
+    <HeaderWrap>
+      <Link to="/">
+        <Logo src={logo} alt="Sweet Ten" />
+      </Link>
+      <Wrap>
+        <Nav />
+        <User>김이삭 ・ 메이트</User>
+        <LogOutButton />
+      </Wrap>
+    </HeaderWrap>
   );
 };
 
--- src/shared/Layout.jsx
+++ src/shared/Layout.jsx
@@ -2,7 +2,10 @@ import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 import Header from "./header/Header";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  width: 1440px;
+  margin: 0 auto 100px;
+`
 
 const Layout = () => {
   return (
