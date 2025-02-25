9049a105c3b55ce1d863af9cf89621c5baa4d7f2
nani <pha3113@naver.com>
2025-02-25 11:24:51 +0900

    feat : input, login input 수정 및 guide 추가

--- src/shared/components/input/Input.jsx
+++ src/shared/components/input/Input.jsx
@@ -1,57 +1,40 @@
-import { useState } from "react";
 import styled from "styled-components";
 
 const InputFieldWrapper = styled.input`
-  width: 100%; // size fix 아닌 100%로 수정
-  height: auto; // size fix 아닌 auto로 수정
-  padding: 10px 10px;
+  width: 100%;
+  padding: 10px;
   border-radius: 8px;
   box-sizing: border-box;
-  color: var(--Text-Primary);
+  color: var(--text-primary);
   font-size: var(--font-size-primary);
   font-weight: 400;
-  line-height: 24px; /* 150% */
-  border: 1px solid
-    ${(props) =>
-      props.isFocused
-        ? "var(--accent)" // 클릭 상태일 때
-        : props.isSubmitted
-        ? "var(--disabled)" // 제출된 상태일 때
-        : "var(--disabled)"}; // 기본 비활성 상태
-  background-color: ${(props) =>
-    props.isFocused
-      ? "var(--white)" // 클릭 상태일 때
-      : props.isSubmitted
-      ? "var(--white)" // 제출된 상태일 때
-      : "var(--background-color-3)"}; // 기본 비활성 상태
-  color: ${(props) =>
-    props.isFocused
-      ? "var(--Text-Primary)" // 클릭 상태일 때
-      : props.isSubmitted
-      ? "var(--Text-Primary)" // 제출된 상태일 때 (글씨 색상)
-      : "var(--disabled)"}; // 기본 비활성 상태
-  pointer-events: ${(props) =>
-    props.isSubmitted ? "none" : "auto"}; // 제출된 상태에서는 수정 불가
+  line-height: 24px;
+  background-color: var(--background-color-3);
+  border: 1px solid var(--disabled);
+
+  &:focus {
+    border-color: var(--accent);
+    background-color: var(--white);
+  }
+
+  &:disabled {
+    background-color: var(--white);
+    pointer-events: none;
+  }
 `;
 
 const InputField = ({
-  isSubmitted,
+  disabled,
   placeholder = "필요에 의한 내용을 바꿔주세요",
 }) => {
-  const [isFocused, setIsFocused] = useState(false);
-
-  const handleFocus = () => setIsFocused(true);
-  const handleBlur = () => setIsFocused(false);
-
   return (
-    <InputFieldWrapper
-      type="text"
-      onFocus={handleFocus}
-      onBlur={handleBlur}
-      isFocused={isFocused}
-      isSubmitted={isSubmitted}
-      placeholder={placeholder}
-    />
+    <>
+      <InputFieldWrapper
+        type="text"
+        disabled={disabled}
+        placeholder={placeholder}
+      />
+    </>
   );
 };
 
--- src/shared/components/input/LoginInput.jsx
+++ src/shared/components/input/LoginInput.jsx
@@ -6,20 +6,15 @@ const InputWrapper = styled.div`
 `;
 
 const StyledInput = styled.input`
-  padding: 0 0 20px 0;
-  border: none;
-  width: 100%;
-  max-width: 100%;
-  height: auto;
-  font-size: var(--font-size-title-small);
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
-  letter-spacing: -0.36px;
+  padding-bottom: 20px;
   margin-bottom: 20px;
+  width: 100%;
+  font-size: var(--font-size-title-small);
+  font-weight: 700;
+  letter-spacing: -0.36px;
   color: var(--text-primary);
-  box-sizing: border-box;
   border-bottom: 2px solid var(--text-disabled-2);
+
   &::placeholder {
     color: var(--text-disabled-2);
   }
@@ -41,4 +36,3 @@ const LoginInput = ({ name, value, onChange, placeholder, type }) => {
 };
 
 export default LoginInput;
-//export default { InputWrapper, Input };
