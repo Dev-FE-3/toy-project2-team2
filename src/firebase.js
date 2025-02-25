be132878c71f6117ba007fb396bdcc4610ed70c5
jiyoon04 <94222592+jiyoon04@users.noreply.github.com>
2025-02-25 14:26:43 +0900

    Feat : storage, db import 수정

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCa_-pBEDd_lEnQKR5u92P4627mKrkl8PU",
@@ -14,4 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
-export const db = getFireStore(app);
+export const db = getFirestore(app);
