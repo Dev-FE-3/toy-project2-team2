76cd10a522a9b816e1e58c7b36b6921133142830
nani <pha3113@naver.com>
2025-03-04 15:16:13 +0900

    Refactor : 폴더 구조 변경

--- src/pages/salaryAdjustment/SalaryAdjustment.jsx
+++ src/pages/salaryAdjustment/SalaryAdjustment.jsx
@@ -1,4 +1,4 @@
-import PageTitle from "../../shared/components/titles/PageTitle";
+import PageTitle from "../../shared/components/PageTitle";
 
 const SalaryAdjustment = () => {
   return <PageTitle title="정정 신청 / 내역" subtitle="정정 내역" />;
